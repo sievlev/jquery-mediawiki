09f8b118741ac5ba689f53b7b5766591fd2978b3
Stanislav Ievlev <stanislav.ievlev@gmail.com>
2012-11-01 15:46:30 +0400

    fix test function name

--- test/mediawiki_test.js
+++ test/mediawiki_test.js
@@ -1,6 +1,6 @@
 /*global QUnit:false, module:false, test:false, asyncTest:false, expect:false*/
 /*global start:false, stop:false ok:false, equal:false, notEqual:false, deepEqual:false*/
-/*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
+/*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, throws:false*/
 (function($) {
 
 module("MediaWiki tokenize");
